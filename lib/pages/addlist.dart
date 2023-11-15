24bcc2976341c74f0beb4bba16f32564fb3e2c35
victoireMyinda <myndavictoire@gmail.com>
2023-11-15 12:54:47 +0100

    main: add_page_add_task

--- lib/pages/addlist.dart
+++ lib/pages/addlist.dart
@@ -14,6 +14,22 @@ class _AddTaskState extends State<AddTask> {
       appBar: AppBar(
         title: const Text("Add Task"),
       ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          const TextField(
+            decoration: InputDecoration(hintText: "Title"),
+          ),
+          const TextField(
+            decoration: InputDecoration(hintText: "Description"),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 10,
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(onPressed: () {}, child: const Text("Submit"))
+        ],
+      ),
     );
   }
 }
